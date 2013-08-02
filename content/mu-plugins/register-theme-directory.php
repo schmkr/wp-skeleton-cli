1c8f8574b8b057fc491f5ce4a1eb6f215185f26e
schmkr <git@schmkr.nl>
2013-08-02 10:48:46 +0200

    Added the "register-theme-directory.php" mu-plugin.
    This plugin lets Wordpress know that it should use its own themes
    directory, as wel as the one in the root/content/themes dir.

---
+++ content/mu-plugins/register-theme-directory.php
@@ -0,0 +1,4 @@
+<?php
+//these are the original WP themes in wp/wp-content/themes/
+//our themes are in content/themes/ which is automagically (because it is in WP_CONTENT_DIR???)
+register_theme_directory( ABSPATH . 'wp-content/themes/' );
