3e8c9e27428f702485f5053ab167ab25dc03705e
schmkr <git@schmkr.nl>
2013-08-02 13:43:28 +0200

    Updated the install plugins script to add an option for the social plugin
    And disabled the caching plugin, because it was generating errors

--- scripts/install-plugins.sh
+++ scripts/install-plugins.sh
@@ -3,18 +3,21 @@
 # This script assumes you have wp-cli installed.
 # @see http://wp-cli.org/ for installation instructions
 #
+# This script should also be called from the root dir of this project.
 
 # Wordpress Importer
 wp plugin install "wordpress-importer" && wp plugin activate "wordpress-importer"
 
 # WP Super Cache
-wp plugin install "wp-super-cache" && wp plugin activate "wp-super-cache"
+# disabled because of php errors
+# wp plugin install "wp-super-cache" && wp plugin activate "wp-super-cache"
 
 # WordPress SEO by Yoast
 wp plugin install "wordpress-seo" && wp plugin activate "wordpress-seo"
 
 # Social (by MailChimp)
 wp plugin install "social" && wp plugin activate "social"
+wp option set social_aggregate_comments 0 #disable the aggregation of social comments
 
 # Regenerate Thumbnails
 wp plugin install "regenerate-thumbnails" && wp plugin activate "regenerate-thumbnails"
